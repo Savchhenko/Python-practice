599592f1bc761cf93cd61515c2f9ff9e0ea9ee63
Savchhenko <47991015+Savchhenko@users.noreply.github.com>
2019-05-09 10:41:42 +0300

    Added data entry validation

--- pyramid_from_mario.py
+++ pyramid_from_mario.py
@@ -2,10 +2,17 @@ piramide = []
 step = 0
 symbol = str('##')
 
-number_of_steps = int(input('Введите количество ступеней в пирамиде? - '))
+number_of_steps = int(input('Введите количество ступеней в пирамиде? - ')) #Высота пирамиды
+
+if number_of_steps > 23:
+    print("Введите число, которое меньше 23: ")
+    number_of_steps = int(input())
+elif number_of_steps < 1:
+    print("Введите число, которое больше 0: ")
+    number_of_steps = int(input())
 
 while step < number_of_steps:
-    piramide.append(symbol[0])
-    print(symbol.rjust(number_of_steps))
-    step = step + 1
-    symbol = symbol + '#'
+        piramide.append(symbol[0])
+        print(symbol.rjust(number_of_steps))
+        step = step + 1
+        symbol = symbol + '#'
