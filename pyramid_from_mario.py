af239ed05320ef516e026931887b3709c764e36c
Savchhenko <47991015+Savchhenko@users.noreply.github.com>
2019-05-09 11:15:09 +0300

    Final version

--- pyramid_from_mario.py
+++ pyramid_from_mario.py
@@ -1,18 +1,17 @@
-piramide = []
 step = 0
 symbol = str('##')
 
-number_of_steps = int(input('Введите количество ступеней в пирамиде? - ')) #Высота пирамиды
+number_of_steps = int(input('Enter the number of steps in the pyramid? - ')) #the height of the pyramid
 
 if number_of_steps > 23:
-    print("Введите число, которое меньше 23: ")
+    print("Enter a number that is less than 23: ")
     number_of_steps = int(input())
 elif number_of_steps < 1:
-    print("Введите число, которое больше 0: ")
+    print("Enter a number that is greater than 0: ")
     number_of_steps = int(input())
 
 while step < number_of_steps:
-    piramide.append(symbol[0])
     print(symbol.rjust(number_of_steps))
     step = step + 1
     symbol = symbol + '#'
+
